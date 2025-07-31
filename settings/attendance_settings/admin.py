833c849f9e62e5c13bfd28f2bc93f9c349474a92
AkhilaEmergio <akhila@emergiotech.com>
2025-07-31 11:08:29 +0530

    admin register

--- settings/attendance_settings/admin.py
+++ settings/attendance_settings/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+admin.site.register(AttendaceSettings)
+admin.site.register(RegularizationPolicies)
+admin.site.register(WeeklyOff)
+admin.site.register(Shift)
+admin.site.register(RosterShiftSettings)
+admin.site.register(ShiftChangeSettings)
+admin.site.register(SandwichRulesSettings)
+admin.site.register(TimeManagementPolicy)
+admin.site.register(CalculationPolicy)
+admin.site.register(WeeklyOff)
+admin.site.register(AllowedIP)
+admin.site.register(CompOffRules)
+admin.site.register(CompensationRules)
--- settings/employee_settings/admin.py
+++ settings/employee_settings/admin.py
@@ -4,4 +4,4 @@ from settings.employee_settings.models import *
 # Register your models here.
 
 admin.site.register(EmployeeProfileSettings)
-admin.site.register(DocumentSettings)
+admin.site.register(DocumentSetting)
