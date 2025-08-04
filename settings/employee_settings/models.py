18e7993b44824550c78c22bc5a2058954fc02ed6
AkhilaEmergio <akhila@emergiotech.com>
2025-08-04 15:41:37 +0530

    msg

--- settings/employee_settings/models.py
+++ settings/employee_settings/models.py
@@ -4,6 +4,53 @@ from user.models import Organization
 
 User = get_user_model()
 
+def default_generally_showable_fields():
+    return {
+        "email": False,
+        "employee_status": False,
+        "profile_image": False
+    }
+
+def default_officially_showable_fields():
+    return {
+        "employee_code": False,
+        "department": False,
+        "designation": False,
+        "location": False,
+        "joining_date": False,
+        "reporting_manager": False
+    }
+
+def default_contacts_showable_fields():
+    return {
+        "mobile": False,
+        "address": False,
+        "email": False,
+        "linked": False
+    }
+
+def default_other_showable_fields():
+    return {
+        "about_me": False,
+        "skills": False,
+        "attendance_status": False,
+        "employment_type": False,
+        "service_status": False
+    }
+
+def default_mandatory_inputable_fields():
+    return {
+        "job_history": False,
+        "education_details": False,
+        "family_details": False,
+        "bank_details": False,
+        "documents": False,
+        "emergency_contact": False,
+        "certifications": False,
+        "profile_image": False
+    }
+
+
 class EmployeeProfileSettings(models.Model):
     manage_employee_profile = models.CharField(max_length=50, choices=[('allowed', 'allowed'), ('limited', 'limited'), ('blocked', 'blocked')])
     unique_fields = models.JSONField()
@@ -12,16 +59,16 @@ class EmployeeProfileSettings(models.Model):
     approve_required = models.BooleanField(default=False)
     employees_addable = models.BooleanField(default=False)
     filter_search = models.BooleanField(default=False) ## Show only active employees
-    generally_showable_fields = models.JSONField(default={"email": False, "employee_status": False, "profile_image":False})
-    officially_showable_fields = models.JSONField(default={"employee_code": False, "department": False, "designation":False,
-        "location": False, "joining_date": False, "reporting_manager": False})
-    contacts_showable_fields = models.JSONField(default={"mobile": False, "address": False, "email":False, "linked": False})
-    other_showable_fields = models.JSONField(default={"about_me": False, "skills": False, "attendance_status":False,
-        "employment_type": False, "service_status": False})
-    mandatory_inputable_fields = models.JSONField(default={"job_history": False, "education_details": False, "family_details": False,
-        "bank_details": False, "documents": False, "emergency_contact": False, "certifications": False, "profile_image":False})
+    generally_showable_fields = models.JSONField(default=default_generally_showable_fields)
+    officially_showable_fields = models.JSONField(default=default_officially_showable_fields)
+    contacts_showable_fields = models.JSONField(default=default_contacts_showable_fields)
+    other_showable_fields = models.JSONField(default=default_other_showable_fields)
+    mandatory_inputable_fields = models.JSONField(default=default_mandatory_inputable_fields)
+
     organization=models.ForeignKey(Organization,on_delete=models.CASCADE,null=True)
     updated_by = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True, related_name="employee_profile_updated_by")
+
+
 
 class DocumentSetting(models.Model):
     title = models.CharField(max_length=50)
